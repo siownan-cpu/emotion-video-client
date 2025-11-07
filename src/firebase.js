d3d3d3375e72944a1cf8f99fdc974be7e7778b2d
siownan-cpu <siownan@gmail.com>
2025-11-08 07:47:58 +0800

    Update Firebase configuration to use environment variables
    Replaced Firebase configuration with environment variables and removed analytics import.

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqlUkKNE80gKNrAZahWwdpcD2Rjz0hvDU",
-  authDomain: "emotion-video-call.firebaseapp.com",
-  projectId: "emotion-video-call",
-  storageBucket: "emotion-video-call.firebasestorage.app",
-  messagingSenderId: "562036327503",
-  appId: "1:562036327503:web:acb5033a29e39e96ee963a",
-  measurementId: "G-VSZ1N16FJN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
